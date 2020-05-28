aced4ac48a89d47279297ee79f4d8cb6d58b44c9
Matthew Weaver <matthew.weaver1@vodafone.com>
2020-05-28 16:16:06 +0100

    Got MVP working with docker compose just printing sentiment

--- kafka/twitter/producer.py
+++ kafka/twitter/producer.py
@@ -4,9 +4,10 @@ from tweepy import Stream
 from kafka import KafkaProducer
 from json import dumps
 import configparser
+from confluent_kafka import Producer
 
 config = configparser.ConfigParser()
-config.read('../twitter.ini')
+config.read('../../twitter.ini')
 access_token = config['DEFAULT']['access_token']
 access_token_secret = config['DEFAULT']['access_token_secret']
 consumer_key = config['DEFAULT']['consumer_key']
@@ -23,12 +24,13 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print(status)
 
-# kafka = KafkaClient("localhost:9092")
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x: dumps(x).encode('utf-8'))
-l = StdOutListener()
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, l)
-#TODO: Parameterize search term
-stream.filter(track=["covid"])
+if __name__ == "__main__":
+    # kafka = KafkaClient("localhost:9092")
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                             value_serializer=lambda x: dumps(x).encode('utf-8'))
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+    #TODO: Parameterize search term
+    stream.filter(track=["covid"])
